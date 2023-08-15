c3ac489f9cc6f3e422fdbd61a0ea8e54548cf4da
Joseph Kihurani <joseph.kihurani@student.moringaschool.com>
2023-08-15 17:09:20 +0300

    js scripts creator

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+<p id="Weekdayer"></p>
+
+const DaysOfWeek = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday","Saturday"]
+     let size = DaysOfWeek.length;
+document.getElementById("Weekdayer").innerHTML = size;
+let Day=DaysOfWeek[3]
+Day
+/*
+const DaysOfWeek = ["Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday","Saturday"];
+let Day = DaysOfWeek.pop();
+document.getElementById("demo").innerHTML = Day
+*/
+ var Yearborn=prompt("Which year were you born ?")
+ var Monthborn=prompt("Which month were you born ?")
+ var Dateborn=prompt("Which date were you born ?")
+ // input date values and use methods to work on them
+  alert("You were born on "+Dateborn+" "+Monthborn+" "+Yearborn)
+
+                window.alert("vipi mseiya")
