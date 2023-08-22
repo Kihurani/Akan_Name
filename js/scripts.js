53095ac39069173a47d1d25812605c9dd3b00749
Joseph Kihurani <joseph.kihurani@student.moringaschool.com>
2023-08-22 23:35:12 +0300

    comments and code error corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 let btnShow=document.querySelector('button');
-let result = document.querySelector('h3');
+let result = document.querySelector('h5');
 
 btnShow.addEventListener('click',() =>{
     let selected = document.querySelector('input[type="radio"]:checked');
@@ -10,82 +10,162 @@ btnShow.addEventListener('click',() =>{
 
    var  Gender=result.innerText
   
-                     alert("your gender is "+Gender)
-                     alert("For your biodata, ensure you put in as advised");
+          alert("your gender is "+Gender)
+          alert("For your biodata, ensure you put in as advised");
 
-                        var Yearborn=prompt("Which year were you born ? (Input 4 digits,YYYY)")
-                        var Monthborn=prompt("Which month were you born ? (Use digit values between 1 and 12)")
-                       //    alert((Monthborn==='' || isNaN(Monthborn))?"Use digit values between 1 and 12":$'{Monthborn}');
-                        var Dateborn=prompt("Which date were you born ? (Use digit values between 1 and 31)")
+              var Yearborn=prompt("Which year were you born ? (Input 4 digits,YYYY)")
+              var Monthborn=prompt("Which month were you born ? (Use digit values between 1 and 12)")
+              var Dateborn=prompt("Which date were you born ? (Use digit values between 1 and 31)")
 
-                       let MM=Monthborn*1;
+                   let MM=Monthborn*1;  // convert Monthborn string to number
 
+
+                   var YB=(Yearborn*1); // convert Yearborn string to a number
+
+                   var CC= Yearborn.slice(0, 2); // Century year 
+                   var YY= Yearborn.slice(2, 4); // Units year
+
+                   let UY=CC*1 ; // convert Century year string to number
+                  let SY=YY*1 ; // convert Units year string to number
+
+                   switch(YB%4) // Formulate leap year identification code
+                        { 
+                         case 0:
+                           switch(UY%4)
+                                 {
+                                    case 0:
+                                      Ytype="Leap";
+                                     break;
+
+                                    case 1:
+                                       switch(SY%4)
+                                           {
+                                             case 0:
+                                               Ytype="Leap";
+                                             break;
+                                             case 1 :
+                                             case 2 :
+                                             case 3 :
+                                               Ytype="Norm"
+                                           } 
+
+                                    case 2:
+                                     switch(SY%4)
+                                     {
+                                       case 0:
+                                         Ytype="Leap";
+                                       break;
+                                       case 1 :
+                                       case 2 :
+                                       case 3 :
+                                         Ytype="Norm"
+                                     } 
+
+                                    case 3:
+                                     switch(SY%4)
+                                     {
+                                       case 0:
+                                         Ytype="Leap";
+                                       break;
+                                       case 1 :
+                                       case 2 :
+                                       case 3 :
+                                         Ytype="Norm"
+                                     } 
+                               
+                               }
+                         break;
+                         case 1:
+                         case 2:
+                         case 3:
+                               Ytype="Norm";
+
+                        }   
+           
+                                      alert("The year is a type "+Ytype);
+
+ // Each month has a similar weekday code depending on whether it's a leap year or not
                        switch (MM) 
                                      {
                                        case 1:
-                                         RefDat=3;
-                                         MB='January';
+                                           switch(Ytype)
+                                               {
+                                                 case 'Norm' :
+                                                  RefDat=3;
+                                                 break;
+                                                 case 'Leap' :
+                                                  RefDat=4;
+                                                }   
+                                          MB='January';
                                          break;
+
                                        case 2:
-                                         RefDat=28;
+                                           switch(Ytype)
+                                                {
+                                                  case 'Norm' :
+                                                   RefDat=28;
+                                                 break;
+                                                  case 'Leap' :
+                                                   RefDat=29;
+                                                }
                                          MB='February';
                                          break;
+
                                        case 3:
                                          RefDat=14;
                                          MB='March';
                                          break;
+
                                        case 4:
                                          RefDat=4;
                                          MB='April';
                                          break;
+
                                        case 5:
                                          RefDat=9;
                                          MB='May';
                                          break;
+
                                        case 6:
                                          RefDat=6;
                                          MB='June';
                                          break;
+
                                        case 7:
                                          RefDat=11;
                                          MB='July';
                                          break;
+
                                        case 8:
                                          RefDat=8;
                                          MB='August';
                                          break;
+
                                        case 9:
                                          RefDat=5;
                                          MB='September';
                                          break;
+
                                        case 10:
                                          RefDat=10;
                                          MB='October';
                                          break;
+
                                        case 11:
                                          RefDat=7;
                                          MB='November';
                                          break;
+
                                        case 12:
                                          RefDat=12;
                                          MB='December';
-                                         break;
                                      }
-
-          // alert("The months magic Weekday falls on date " + RefDat)
-
 
                          alert("You were born on "+MB+" "+Dateborn+", "+Yearborn)
 
-                              var CC= Yearborn.slice(0, 2);
-                              var YY= Yearborn.slice(2, 4);
-                              // console.log(CC);
+                         
+  // Next process step is to formulate the weekday code
 
-                              // alert("Century year is : "+CC);
-                              // alert("Units year is : "+YY);
-
-                              let UY=CC*1 ;
-                             let SY=YY*1
 
                                    switch (UY) {
                                         case 18:
@@ -108,8 +188,7 @@ btnShow.addEventListener('click',() =>{
                                         case 13:
                                              Operand_1 =0;
                                    }
-                             // alert("The first operand Value is " + Operand_1); //(5 if CC=18; 3 if CC=19; 2 if CC=20; 0 if CC=21) 
-
+  // Formulate the first operand based on the Century year number value geared towards generating the weekday code
                               var   Operand_2=Math.floor(YY/12) // extract the years quotient after dividing by 12
 
                               var Operand_3=YY%12  // extract the years remainder after dividing by 12
@@ -118,15 +197,13 @@ btnShow.addEventListener('click',() =>{
 
                               var  Operand_5=(Operand_1+Operand_2+Operand_3+Operand_4)%7  // add above operands and extract remainder after dividing by 7 to get the years magic weekday value where
 
-                             // alert("First operand is : "+Operand_1) 
-                 //             alert("Second operand is : "+Operand_2) 
-                   //           alert("third operand is : "+Operand_3)
-                     //         alert("fourth operand is : "+Operand_4)
-                       //       alert("Fifth operand is : "+Operand_5)
-                       
-
-
-                                                  
+  //                            alert("First operand is : "+Operand_1) ;
+    //                        alert("Second operand is : "+Operand_2) ;
+      //                        alert("third operand is : "+Operand_3);
+        //                     alert("fourth operand is : "+Operand_4);
+           //                  alert("Fifth operand is : "+Operand_5);
+                     
+                                                 
                                switch (Operand_5)
                                             {
                                               case 0:
@@ -151,68 +228,20 @@ btnShow.addEventListener('click',() =>{
                                               MagWkDay = "Saturday";
                                             }
 
-                           //                 alert("The years magic Weekday is " + MagWkDay)
+ //                                          alert("The years magic Weekday is " + MagWkDay);
+
+   //           var operand_6=(Dateborn-RefDat)+35;
+     //         alert("Operand 6 is Dateborn-months RefDate giving "+operand_6);
+       //       var operand_7=operand_6%7
+         //     alert("Operand 7 is previous value MOD 7 giving "+operand_7);
+           //   var operand_8=operand_7+Operand_5
+             // alert("Operand 8 is previous value and add MagicWkDay code (operand_5) giving "+operand_8);
+       //       var operand_9=operand_8%7
+         //     alert("Operand 9 is is previous value MOD 7 giving "+operand_9);
 
 
-                           var YB=(Yearborn*1);
-                           //                       alert("The multiplied by 1 year number is "+YB);
-                                                 
-                                  switch(YB%4)
-                                       { 
-                                        case 0:
-                                          switch(UY%4)
-                                                {
-                                                   case 0:
-                                                     Ytype="Leap";
-                                                    break;
-                                                   case 1:
-                                                      switch(SY%4)
-                                                          {
-                                                            case 0:
-                                                              Ytype="Leap";
-                                                            break;
-                                                            case 1 :
-                                                            case 2 :
-                                                            case 3 :
-                                                              Ytype="Norm"
-                                                          } 
-                                                   case 2:
-                                                    switch(SY%4)
-                                                    {
-                                                      case 0:
-                                                        Ytype="Leap";
-                                                      break;
-                                                      case 1 :
-                                                      case 2 :
-                                                      case 3 :
-                                                        Ytype="Norm"
-                                                    } 
 
-                                                   case 3:
-                                                    switch(SY%4)
-                                                    {
-                                                      case 0:
-                                                        Ytype="Leap";
-                                                      break;
-                                                      case 1 :
-                                                      case 2 :
-                                                      case 3 :
-                                                        Ytype="Norm"
-                                                    } 
-                                              
-                                              }
-                                        break;
-                                        case 1 :
-                                        case 2:
-                                        case 3:
-                                              Ytype="Norm";
-
-                                       }   
-                          
-                                                     alert("The year is a type "+Ytype);
-
- 
-let WkDayBorn=(((Dateborn-RefDat)%7)+Operand_5)%7  // Dateborn is date provided at input for the month MM provided 
+let WkDayBorn=((((Dateborn-RefDat)+28)%7)+Operand_5)%7  // Dateborn is date provided at input for the month MM provided 
 
 // alert("Weekday code is "+WkDayBorn)
 switch (WkDayBorn) 
